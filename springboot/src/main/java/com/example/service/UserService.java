7f5a9b840f4bbb088799e8dd2696aceb5853e2db
ichigo101101 <1429553410@qq.com>
2024-10-22 17:59:51 +0900

    first

---
+++ springboot/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,49 @@
+package com.example.service;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.example.common.Constants;
+import com.example.common.enums.ResultCodeEnum;
+import com.example.common.enums.RoleEnum;
+import com.example.entity.User;
+import com.example.exception.CustomException;
+import com.example.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    public void add(User user) {
+        // 校验用户名是否存在
+        User dbUser = userMapper.selectByUsername(user.getUsername());
+        if (ObjectUtil.isNotEmpty(dbUser)) {
+            throw new CustomException(ResultCodeEnum.USER_EXIST_ERROR);
+        }
+        if (ObjectUtil.isEmpty(user.getPassword())) {
+            user.setPassword(Constants.USER_DEFAULT_PASSWORD);
+        }
+        user.setRole(RoleEnum.USER.name());
+        userMapper.insert(user);
+    }
+
+    public PageInfo<User> selectPage(User user, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<User> list = userMapper.selectAll(user);
+        return PageInfo.of(list);
+    }
+
+    public void update(User user) {
+        userMapper.updateById(user);
+    }
+
+    public void delete(Integer id) {
+        userMapper.deleteById(id);
+    }
+}
